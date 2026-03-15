a25f68339960e9e0462aa2f6a5cf7a327342f8e8
usman <usmankhalil.twig@gmail.com>
2026-03-15 09:31:52 +0100

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/randomdinner/',   // MUST match your repo name exactly (lowercase)
+  base: '/randomdinner/',
   plugins: [react()],
 })
